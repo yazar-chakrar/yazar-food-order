76b6ad748e126a0a5c6c13c25427cd76f137175c
Mohammed El Amin CHAKRAR <37815501+yazar-chakrar@users.noreply.github.com>
2022-12-19 01:14:40 +0100

    Adding controllers

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,7 @@
 // database related modules
 module.exports = {
   databaseConnection: require("./connection"),
+  CustomerController: require("./controllers/customer-controller"),
+  ProductController: require("./controllers/product-controller"),
+  ShoppingController: require("./controllers/shopping-controller"),
 };
